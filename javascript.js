57479db120af78894f0577b3b2594414d193bbae
Yosef Ben Yehuda <48494974+YoderBy@users.noreply.github.com>
2023-06-28 01:08:32 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,131 +1,228 @@
-
 var cards_clicked = 0;
 var first_card = -1;
+var second_card = -1;
 var comparing = false;
+var distance = 300;
 
-function cardClicked(card) {
-    if (window.cards_clicked == 0) {
-        window.first_card = card
-        window.cards_clicked = 1;
-    }
-    else {
-        compareCard(window.first_card, card);
-        window.cards_clicked = 0;
-    }
-}
+function cardClicked(card, Choice_state) {
+  state = getValue();
+  console.log(state);
+  if (state == 2) {
 
-function compareCard(card1, card2) {
+    if (cards_clicked == 0) {
+      first_card = card;
+      cards_clicked = 1;
+    } else {
+      compareTwoCard(first_card, card);
+      cards_clicked = 0;
+    }
+    }
+    
+  if (state == 3) {
+      if (cards_clicked == 2)
+      {
+        compareThreeCard(first_card, second_card, card);
+        cards_clicked = 0;
+        console.log('compared3');
+      }
+
+     else if (cards_clicked == 1) {
+          second_card = card;
+          cards_clicked = 2;
+          console.log(`num of ${cards_clicked}`);
+        }
+     else if (cards_clicked == 0) {
+          first_card = card;
+          cards_clicked = 1;
+          console.log(`num of ${cards_clicked}`);
+        }
+     
+    }
+  }
+
+  function compareThreeCard(card1, card2, card3) {
     card1_value = card1.firstChild.src;
     card2_value = card2.firstChild.src;
-    window.comparing = true;
+    card3_value = card3.firstChild.src;
+
+    comparing = true;
+
+    if (card1_value == card2_value && card2_value == card3_value) {
+      card1.firstChild.style.opacity = 1;
+      card2.firstChild.style.opacity = 1;
+      card3.firstChild.style.opacity = 1;
+
+      console.log(`match!   ${card1_value}`);
+      card1.setAttribute('solved', 'true');
+      card2.setAttribute('solved', 'true');
+      card3.setAttribute('solved', 'true');
+
+      first_card = -1;
+      second_card = -1;
+      comparing = false;
+    } else {
+      const myTimeout = setTimeout(function() {
+        changeBgColor(card1);
+        changeBgColor(card2);
+        changeBgColor(card3);
+        comparing = false;
+      }, 1000);
+
+    }
+  }
+
+  function compareTwoCard(card1, card2) {
+    card1_value = card1.firstChild.src;
+    card2_value = card2.firstChild.src;
+    comparing = true;
     if (card1_value == card2_value) {
-        card1.firstChild.style.opacity = 1;
-        card2.firstChild.style.opacity = 1;
-        console.log(`match!   ${card1_value}`)
-        card1.setAttribute('solved', 'true');
-        card2.setAttribute('solved', 'true');
-        window.first_card = -1;
+      card1.firstChild.style.opacity = 1;
+      card2.firstChild.style.opacity = 1;
+
+      console.log(`match!   ${card1_value}`);
+      card1.setAttribute('solved', 'true');
+      card2.setAttribute('solved', 'true');
+      window.first_card = -1;
+      window.comparing = false;
+    } else {
+      const myTimeout = setTimeout(function() {
+        changeBgColor(card1);
+        changeBgColor(card2);
         window.comparing = false;
-    }
-
-    else {
-        const myTimeout = setTimeout(function () {
-            changeBgColor(card1);
-            changeBgColor(card2);
-            window.comparing = false;
-        }, 1000);
+      }, 1000);
 
     }
-}
+  }
 
-function clicked(element) {
-    if (window.comparing == false) {
-        cardClicked(element);
-        changeBgColor(element);
+  function clicked(element) {
+    if (comparing == false) {
+      cardClicked(element);
+      changeBgColor(element);
     }
-}
+  }
 
+  function count(arr, target) {
+    // counts the appearnce of target in arr
 
-function generateTable(row_num, col_num) {
-    table = document.getElementById('game_table');
-    table.innerHTML = "";
-    for (let i = 0; i < row_num; i++) {
-        new_row = document.createElement('tr');
-        for (let j = 0; j < col_num; j++) {
-            new_col = document.createElement('td');
-            new_col.setAttribute('solved', 'false');
-            new_img = document.createElement('img');
-            rnd = Math.floor(Math.random() * 10);
-            var img_w = 68 + rnd*5;
-            var img_h = 108 + rnd*5;
-            new_img.src =  `https://placekitten.com/${img_w}/${img_h}`;
+    var counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == target) {
+        counter++;
+      }
+    }
+    return counter;
+  }
 
+  function getValue() {
+    //gets the current value of the match button
+    if (document.getElementById('value-3').checked) {
+      return 3;
+    } else {
+      return 2;
+    }
+  }
 
-            new_img.style.opacity = 0;
-            new_col.appendChild(new_img);
-            new_col.setAttribute('onclick', 'clicked(this)');
-            new_row.appendChild(new_col);
+  function generateArray(table_size, twoORthree) {
+    random_numbers = [];
+    times_appeared = {};
+
+    while (random_numbers.length < table_size) {
+      rnd = Math.floor(Math.random() * table_size / twoORthree);
+      if (times_appeared[rnd]) {
+        if (times_appeared[rnd] < twoORthree) {
+          times_appeared[rnd]++;
+          random_numbers.push(rnd);
         }
-        table.appendChild(new_row);
+      } else {
+        times_appeared[rnd] = 1;
+        random_numbers.push(rnd);
+      }
     }
-}
+    return random_numbers;
+  }
 
-function test() {
+  function generateTable(row_num, col_num) {
+
+    numofchoice = getValue();
+    random_numbers = generateArray(row_num * col_num, numofchoice);
+
+    document.getElementById('game_table').innerHTML = "";
+    table = document.getElementById('game_table');
+    var counter = 0;
+
+    for (let i = 0; i < row_num; i++) {
+      new_row = document.createElement('tr');
+      for (let j = 0; j < col_num; j++) {
+        new_col = document.createElement('td');
+        new_col.setAttribute('solved', 'false');
+        new_img = document.createElement('img');
+        //new_col.innerHTML = `${random_numbers[counter]}`;
+        new_img.src =  `https://placekitten.com/200/300?image=${random_numbers[counter]}`;
+        new_img.style.opacity = 0;
+        counter += 1;
+        console.log(i + j);
+        new_col.appendChild(new_img);
+
+        new_col.setAttribute('onclick', 'clicked(this)');
+        new_row.appendChild(new_col);
+      }
+      table.appendChild(new_row);
+    }
+  }
+
+  function test() {
     new_text = document.createElement('p');
     new_text.innerHTML = document.getElementById('text_to_add').value;
     document.body.appendChild(new_text);
-}
+  }
 
-
-function changeBgColor(element) {
+  function changeBgColor(element) {
     isSolved = element.getAttribute('solved');
     if (isSolved == 'false') {
-        state = element.firstChild.style.opacity;
-        if (state == '' || state == 0) {
-            element.firstChild.style.opacity = 1;
-        }
+      state = element.firstChild.style.opacity;
+      //state = element.style.opacity;
 
-        if (state == 1) {
-            element.firstChild.style.opacity= 0;
-        }
+      if (state == '' || state == 0) {
+       element.firstChild.style.opacity = 1;
+       // element.style.opacity = 1;
+      }
+
+      if (state == 1) {
+        element.firstChild.style.opacity = 0;
+        //element.style.opacity = 1;
+      }
+    } else {
+
     }
-    else {
 
-    }
+  }
 
-}
-function startTimer() {
-    console.log("dd");
-    setInterval(function () {
-        time = parseInt(document.getElementById("timer").innerHTML) + 1;
-        document.getElementById("timer").innerHTML = time;
-    }, 1000);
-}
 
-function startTimerBack() {
-    timer1 = setInterval(function () {
-        time = parseInt(document.getElementById("timer1").innerHTML) - 1;
-        document.getElementById("timer1").innerHTML = time;
+  function startTimerBack(time) {
+    clearInterval(timer1);
+    timer1 = setInterval(function() {
+      var minutes = parseInt(time / 60);
+      var seconds = parseInt(time % 60);
+      document.getElementById('timer1').innerHTML = `0${minutes} : ${seconds}`;
 
-        if (time == 0) {
-            document.body.style.backgroundColor = 'red';
-            clearInterval(timer1);
-            grth();
-        }
-
+      if (minutes == 0 && seconds == 0) {
+        document.body.style.backgroundColor = 'red';
+        clearInterval(timer1);
+        flash();
+      }
+      time = time - 1;
     }, 100);
-}
-function flash() {
+  }
+
+  function flash() {
     state = true;
-    setInterval(function () {
-        if (state) {
-            document.body.style.backgroundColor = 'white';
-            state = false;
-        }
-        else {
-            document.body.style.backgroundColor = 'red';
-            state = true;
-        }
-    }
-        , 100);
-}
+    setInterval(function() {
+      if (state) {
+        document.body.style.backgroundColor = 'white';
+        state = false;
+      } else {
+        document.body.style.backgroundColor = 'red';
+        state = true;
+      }
+    }, 100);
+  }
